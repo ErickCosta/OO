9036145dd3b1ee96583e3269529ee878f3f0a57e
Erick Costa <erick.costa.ti@gmail.com>
2019-02-19 19:08:48 -0300

    Novo arquivo da aula de OO (parses)

---
+++ src/Parse.java
@@ -0,0 +1,18 @@
+
+public class Parse {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a = 0;
+		
+		String b = String.valueOf(a);
+		
+		String c = "200";
+		
+		int o = Integer.parseInt(c);
+		
+
+	}
+
+}
